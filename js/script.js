5b854ffd024ca7c3e49d44d5dd5813d23c506074
KlaraAB <Kmrazkova@hotmail.com>
2024-05-08 07:48:08 -0600

    Changes JS-4-5

--- js/script.js
+++ js/script.js
@@ -4,3 +4,18 @@ mainTitle.innerText = "Yay, JavaScript!";
 var body = document.querySelector("body");
 body.style.backgroundColor = "#FEF2F1";
 
+var firstListItem = document.querySelector("ul li");
+firstListItem.style.textTransform = "uppercase";
+
+var moreReasons = document.querySelector(".more-reasons");
+moreReasons.style.fontSize = "2.5em";
+
+var whyJS = document.querySelector("h3")
+whyJS.innerHTML = 'Why learn <span class="highlight"> JavaScript</span>?';
+var highlight = document.querySelector(".highlight");
+highlight.style.backgroundColor = "#ffff82";
+
+var mainImage = document.querySelector("img");
+mainImage.src = "img/js-code.png";
+mainImage.alt = "JavaScript code example";
+ 
